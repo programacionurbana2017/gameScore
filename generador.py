1f1cedbe0e2dab762ff242aef36c277419ed5ae0
SebastianSBGR1998 <sebastianamezquitaleon@gmail.com>
2017-05-20 23:14:36 -0500

    coo

--- generador.py
+++ generador.py
@@ -13,19 +13,40 @@ class Generador:
 	
 	def Tabla(self,tabla):
 		contador = 1
-		tabla1 = '''	<tr class="titulo">
+		tabla1 = '''	<tr class="titulo1">
 			<td class="titulo1">POSICION</td>
-			<td class="titulo1">NOMBRE</td>
+			<td class="titulo1" >NOMBRE</td>
 			<td class="titulo1">PUNTUACION</td>
 			<td class="titulo1">TIEMPO JUGADO</td>
 			<td class="titulo1">NACIONALIDAD</td></tr>'''
 		codigo = ""
 		for i in tabla:
-			codigo = codigo + '''<tr class ="">'''
-			codigo = codigo + ''' <td> ''' + str(contador) + '''</td>'''
+			if contador == 1:
+				codigo = codigo + '''<tr>'''
+				codigo = codigo + ''' <td class = "posicionuno"> ''' + str(contador) + '''.</td>'''
+			else:
+				if contador == 2:
+					codigo = codigo + '''<tr>'''
+					codigo = codigo + ''' <td class = "posiciondos"> ''' + str(contador) + '''.</td>'''
+				else:
+					if contador == 3:
+						codigo = codigo + '''<tr >'''
+						codigo = codigo + ''' <td class = "posiciontres"> ''' + str(contador) + '''.</td>'''
+					else:
+						codigo = codigo + '''<tr class ="comun">'''
+						codigo = codigo + ''' <td class="comun"> ''' + str(contador) + '''.</td>'''
 			
 			for j in i.split(","):
-				 codigo = codigo + '''<td class="">''' + j +  "</td>" 
+				if contador == 1:
+					codigo = codigo + '''<td class="posicionuno">''' + j +  "</td>"
+				else:
+					if contador == 2:
+						codigo = codigo + '''<td class="posiciondos">''' + j +  "</td>"
+					else:
+						if contador == 3:
+							codigo = codigo + '''<td class="posiciontres">''' + j +  "</td>"
+						else:	
+							codigo = codigo + '''<td class="comun">''' + j +  "</td>" 
 			
 			contador = contador + 1	 
 		codigo = codigo + "</tr>"
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ class goodAndDevil(object):
 	<!DOCTYPE html>
 	<html lang="en">
 	<head>
-	<link rel="stylesheet" href="static/css/student.css"> 
+	<script src="static/js/js.js"></script>
+	<link rel="stylesheet" href="static/css/student1.css"> 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -54,6 +55,7 @@ class goodAndDevil(object):
             <div >
                 <div>
                     <div class="col-lg-12 text-center">
+					
                     """    
 		text2 = """				
 					
